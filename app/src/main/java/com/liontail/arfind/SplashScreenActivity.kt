8f24756545a6bea18337dcc13100d2f1876aa92e
LiontailSolution <liontailsolutions@gmail.com>
2024-08-27 17:42:12 -0300

    FEAT Splash navegacion basica

--- app/src/main/java/com/liontail/arfind/SplashScreenActivity.kt
+++ app/src/main/java/com/liontail/arfind/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.liontail.arfind
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+                val splashTimeOut = 3000L
+                val handler = Handler(Looper.getMainLooper())
+                handler.postDelayed({
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }, splashTimeOut)
+        }
     }
-}
