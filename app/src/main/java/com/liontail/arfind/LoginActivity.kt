e84e31b068b08383ee07211857dfd84e972f0e44
LiontailSolution <liontailsolutions@gmail.com>
2024-08-27 17:41:57 -0300

    FEAT Login navegacion basica

--- app/src/main/java/com/liontail/arfind/LoginActivity.kt
+++ app/src/main/java/com/liontail/arfind/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.liontail.arfind
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnLoginIngresar = findViewById<Button>(R.id.btn_login_ingresar)
+
+        btnLoginIngresar.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
