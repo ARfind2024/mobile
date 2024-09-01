c33d8e14a2c10de5126507b74fd3097b4b098e43
LiontailSolution <liontailsolutions@gmail.com>
2024-08-31 21:56:04 -0300

    MOD Main y Login

--- app/src/main/java/com/liontail/arfind/LoginActivity.kt
+++ app/src/main/java/com/liontail/arfind/LoginActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.liontail.arfind.utils.NetworkUtils
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        if (!NetworkUtils.isNetworkAvailable(this)) {
+            NetworkUtils.redirectToNoInternetActivity(this)
+        }
 
         val btnLoginIngresar = findViewById<Button>(R.id.btn_login_ingresar)
 
