d4c989852a5f4983269026e3e5273a39ec492bdf
LiontailSolution <liontailsolutions@gmail.com>
2024-09-01 15:06:31 -0300

    FEAT MapFragment

--- app/src/main/java/com/liontail/arfind/fragments/MapFragment.kt
+++ app/src/main/java/com/liontail/arfind/fragments/MapFragment.kt
@@ -1,61 +1,27 @@
 package com.liontail.arfind.fragments
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Bundle
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.liontail.arfind.R
 
 class MapFragment : Fragment() {
 
-    private val LOCATION_PERMISSION_REQUEST_CODE = 1
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-
     private val callback = OnMapReadyCallback { googleMap ->
-        // Verifica si los permisos de ubicación han sido concedidos
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
 
-            try {
-                googleMap.isMyLocationEnabled = true
-
-                // Obtiene la ubicación actual del dispositivo y mueve la cámara
-                fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
-                    if (location != null) {
-                        val currentLatLng = LatLng(location.latitude, location.longitude)
-                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
-                    } else {
-                        // Maneja el caso cuando la ubicación es null
-                        // Por ejemplo, mostrar un mensaje al usuario
-                    }
-                }.addOnFailureListener { e ->
-                    e.printStackTrace()
-                    // Maneja la falla al obtener la ubicación
-                }
-            } catch (e: SecurityException) {
-                e.printStackTrace()
-                // Maneja la excepción de seguridad
-            }
-        } else {
-            // Solicita permisos de ubicación si no han sido concedidos
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
-        }
+        val buenosAires = LatLng(-34.6037, -58.3816)  // Coordenadas de Buenos Aires
+        googleMap.addMarker(MarkerOptions().position(buenosAires).title("Marker in Buenos Aires"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(buenosAires, 10f))  // Ajusta el zoom si es necesario
     }
 
     override fun onCreateView(
@@ -68,31 +34,7 @@ class MapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        // Inicializa el FusedLocationProviderClient
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-
-        setUpMap()
-    }
-
-    private fun setUpMap() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-    }
-
-    // Maneja la respuesta de solicitud de permisos
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                // Permiso concedido, configura el mapa
-                setUpMap()
-            } else {
-                // Permiso denegado, maneja la situación según lo necesites
-            }
-        }
     }
 }
