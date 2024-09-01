2eb4a11abc72f5a122d5ae058bf8948a8822a8b5
LiontailSolution <liontailsolutions@gmail.com>
2024-09-01 12:36:03 -0300

    FEAT Adapter de planes

--- app/src/main/java/com/liontail/arfind/MainActivity.kt
+++ app/src/main/java/com/liontail/arfind/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.liontail.arfind.fragments.HomeFragment
 import com.liontail.arfind.utils.NetworkUtils
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         if (!NetworkUtils.isNetworkAvailable(this)) {
             NetworkUtils.redirectToNoInternetActivity(this)
         }
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNav.setOnItemSelectedListener { item -> handleItemSelected(item.itemId) }
+
+
         val  linearCampana = findViewById<LinearLayout>(R.id.linear_campana)
 
         linearCampana.setOnClickListener{
@@ -20,10 +27,31 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
-
-
-
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
+        }
 
     }
+
+    private fun handleItemSelected(itemId: Int): Boolean {
+        val selectedFragment = when (itemId) {
+            R.id.action_home -> HomeFragment()
+            //R.id.action_pedidos -> MapFragment()
+            //R.id.action_perfil -> MasFragment()
+            else -> null
+        }
+        selectedFragment?.let {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, it)
+                .commit()
+            return true
+        }
+        return false
+    }
+
+
+
+
 }
