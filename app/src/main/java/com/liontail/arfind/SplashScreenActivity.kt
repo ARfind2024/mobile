3491e6b37a95029c1f1d143003e1754c8500a2a0
LiontailSolution <liontailsolutions@gmail.com>
2024-08-31 22:57:21 -0300

    FIX SplashScreenActivity

--- app/src/main/java/com/liontail/arfind/SplashScreenActivity.kt
+++ app/src/main/java/com/liontail/arfind/SplashScreenActivity.kt
@@ -13,17 +13,20 @@ import com.liontail.arfind.firebase.presenter.SharedReferencesPresenter
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
         SharedReferencesPresenter.cargarSharedReferences { success ->
             if (success) {
                 val splashTimeOut = 2000L
                 val handler = Handler(Looper.getMainLooper())
                 handler.postDelayed({
-                    startActivity(Intent(this, MainActivity::class.java))
+                    startActivity(Intent(this, LoginActivity::class.java))
                     finish()
                 }, splashTimeOut)
             } else {
                 Toast.makeText(this, "Error al cargar las referencias compartidas", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
 }
