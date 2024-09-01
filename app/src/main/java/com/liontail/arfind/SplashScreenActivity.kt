ee18f3de5e0f1095f5762209a96deefdd77fdc2c
LiontailSolution <liontailsolutions@gmail.com>
2024-08-31 22:33:44 -0300

    FEAT  SplashScreenActivity + SharedReferencesPresenter

--- app/src/main/java/com/liontail/arfind/SplashScreenActivity.kt
+++ app/src/main/java/com/liontail/arfind/SplashScreenActivity.kt
@@ -1,21 +1,29 @@
 package com.liontail.arfind
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
+import com.liontail.arfind.firebase.presenter.SharedReferencesPresenter
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-                val splashTimeOut = 3000L
+        SharedReferencesPresenter.cargarSharedReferences { success ->
+            if (success) {
+                val splashTimeOut = 2000L
                 val handler = Handler(Looper.getMainLooper())
                 handler.postDelayed({
-                    startActivity(Intent(this, LoginActivity::class.java))
+                    startActivity(Intent(this, MainActivity::class.java))
                     finish()
                 }, splashTimeOut)
+            } else {
+                Toast.makeText(this, "Error al cargar las referencias compartidas", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+}
