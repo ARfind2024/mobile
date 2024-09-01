71601c8bcbb75566cabf282f591d52e6e3a3f0d3
LiontailSolution <liontailsolutions@gmail.com>
2024-09-01 15:58:48 -0300

    FEAT añadi el txt para registrarse

--- app/src/main/java/com/liontail/arfind/LoginActivity.kt
+++ app/src/main/java/com/liontail/arfind/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.liontail.arfind.utils.NetworkUtils
 
 class LoginActivity : AppCompatActivity() {
@@ -16,11 +17,18 @@ class LoginActivity : AppCompatActivity() {
         }
 
         val btnLoginIngresar = findViewById<Button>(R.id.btn_login_ingresar)
+        val txtRegistrar = findViewById<TextView>(R.id.txtRegistrar)
 
         btnLoginIngresar.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+        txtRegistrar.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
