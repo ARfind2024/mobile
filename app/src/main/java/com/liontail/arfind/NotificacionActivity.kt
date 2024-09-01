359b03260ef68b931d6170537d106e7733d30c34
LiontailSolution <liontailsolutions@gmail.com>
2024-08-31 21:56:22 -0300

    FEAR NotificacionActivity

---
+++ app/src/main/java/com/liontail/arfind/NotificacionActivity.kt
@@ -0,0 +1,11 @@
+package com.liontail.arfind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotificacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notificacion)
+    }
+}
